ba630651ada166b23e6f6bf61646a55925db78b8
sgrA77 <subakmukja86@naver.com>
2026-09-11 16:59:00 +0900

    Update TEST001.py

--- TEST001.py
+++ TEST001.py
@@ -20,12 +20,13 @@ stocks = {
 
     # 한국 주식 추가
     "Samsung": "005930.KS",
-    "SKHynix": "000660.KS"
+    "SKHynix": "000660.KS",
 
     # 기타 자산
     "Gold": "GC=F",
-    "Crude Oil": "CL=F"
+    "Crude Oil": "CL=F",
 
+    # 암호 화폐
     "Bitcoin": "BTC-USD"
 }
 
